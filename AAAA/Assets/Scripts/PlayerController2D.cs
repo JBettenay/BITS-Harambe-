5741cd05d6965c910603d21fdb210c0a9cf0bb0b
s3501497 <s3501497@student.rmit.edu.au>
2016-11-13 11:16:22 +1000

    Brett Commit
    Brett Commit

---
+++ AAAA/Assets/Scripts/PlayerController2D.cs
@@ -0,0 +1,76 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class PlayerController2D : MonoBehaviour {
+
+    public int moveSpeed;
+    public int jumpHeight;
+    public int extraJumps;
+    public int currentPLayerHealth;
+    public int maxPlayerHealth;
+
+    public Transform groundPoint;
+    public float radius;
+    public LayerMask groundMask;
+
+
+    bool isGrounded;
+    Rigidbody2D rb2D;
+    int originalExtraJumps;
+
+
+	void Start () {
+        rb2D = GetComponent<Rigidbody2D>();
+        originalExtraJumps = extraJumps;
+        currentPLayerHealth = maxPlayerHealth;
+	}
+	
+	
+	void Update () {
+	
+        Vector2  moveDir = new Vector2(Input.GetAxisRaw("Horizontal") * moveSpeed, rb2D.velocity.y);
+        rb2D.velocity = moveDir;
+
+        isGrounded = Physics2D.OverlapCircle(groundPoint.position, radius, groundMask);
+
+        if (Input.GetAxisRaw("Horizontal") == 1)
+        {
+            transform.localScale = new Vector3(1, 1, 1);
+        }
+            else if(Input.GetAxisRaw("Horizontal") == -1)
+        {
+            transform.localScale = new Vector3(-1, 1,1);
+        }
+
+
+        if(Input.GetKeyDown(KeyCode.Space) && extraJumps != 0)
+        {
+            rb2D.AddForce(new Vector2(0, jumpHeight));
+            extraJumps--;
+        }
+
+        if(isGrounded)
+        {
+            extraJumps = originalExtraJumps;
+        }
+        
+
+        if(currentPLayerHealth <= 0)
+        {
+            Die();
+        }
+
+	}
+
+    void OnDrawGizmos()
+    {
+        Gizmos.color = Color.green;
+        Gizmos.DrawWireSphere(groundPoint.position, radius);
+    }
+
+    void Die()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+}
